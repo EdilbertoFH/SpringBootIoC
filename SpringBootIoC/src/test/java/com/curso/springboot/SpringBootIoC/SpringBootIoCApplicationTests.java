169c72d3840291361de421ebfe0a60d0b4ba1e79
Edilberto Flores <edilbertof423@gmail.com>
2025-02-09 15:15:13 -0600

    Logro: Crear un Bean con @Configuration y @Bean e inyectarlo en nuestra clace principal utilizando @Autowired

---
+++ SpringBootIoC/src/test/java/com/curso/springboot/SpringBootIoC/SpringBootIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.SpringBootIoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
