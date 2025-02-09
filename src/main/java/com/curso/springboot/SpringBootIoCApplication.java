f0bf7c61772db7a66e3edd1137ed8ebf2a6077c4
Edilberto Flores <edilbertof423@gmail.com>
2025-02-09 16:13:13 -0600

    Logro: Aprendimos a crear Beans y dependencia entre ellos - update 2

--- src/main/java/com/curso/springboot/SpringBootIoCApplication.java
+++ src/main/java/com/curso/springboot/SpringBootIoCApplication.java
@@ -1,13 +1,46 @@
-package com.curso.springboot.SpringBootIoC;
+package com.curso.springboot;
 
+import com.curso.springboot.repository.ProductRepositoryA;
+import com.curso.springboot.repository.ProductRepositoryB;
+import com.curso.springboot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootIoCApplication {
+public class SpringBootIoCApplication implements CommandLineRunner {
+	//Instanciacion de objetos almancenados en la configuracion de BEANS manejados directamente por el contenedor de SPRING
+	@Autowired
+	private ProductService productService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIoCApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World con la interfaz comand line runer");
+
+	}
+
+	@Bean
+	public CommandLineRunner comandoGenerico() {
+		return (args) -> {
+			System.out.println("Hello World con la interfaz comando generico Expresion Landa" );
+
+		};
+	}
+
+	//Probando instanciacion de dependencias con @Autowired
+	@Bean
+	public CommandLineRunner comandoUtilizandoAutowured() {
+		return args -> {
+			System.out.println("PRODUCTSERVICE");
+			productService.save("Control Remoto");
+			productService.remove("Mouse USB Generico");
+		};
+	}
 }
