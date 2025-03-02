f12033bc3aeae414e5ece2dc839255f4ab2e9be0
Edilberto Flores <edilbertof423@gmail.com>
2025-03-02 13:49:52 -0600

    Logro: Crear y comprender un circulo de dependencias de Springboot y utilizar la anotacion @Lazy

---
+++ src/main/java/com/curso/springboot/circular/dependencies/ComponentB.java
@@ -0,0 +1,12 @@
+package com.curso.springboot.circular.dependencies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentB {
+
+    @Autowired
+    private ComponentC componentC;
+
+}
